594d19e2031f6e80d15eb48a895b93f72c8e853c
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-04 01:11:06 +0000

    commit

--- src/main/java/dev/elma/view/IOC_IOD_FromScratche_Dynamic.java
+++ src/main/java/dev/elma/view/IOC_IOD_FromScratche_Dynamic.java
@@ -1,7 +1,15 @@
 package dev.elma.view;
 
-public class IOC_IOD_FromScratche_Dynamic {
-    public static void main(String[] args) {
+import dev.elma.dao.IDAO;
 
+import java.io.File;
+import java.util.Scanner;
+
+public class IOC_IOD_FromScratche_Dynamic {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner=new Scanner(new File("config.txt"));
+        Class daoClass=Class.forName(scanner.nextLine());
+        IDAO o =(IDAO) daoClass.getConstructor().newInstance();
+        System.out.println(o.getData());
     }
 }
