60da401a92d078709c44f2a8f974e5eaa695b8dd
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-04 10:00:25 +0000

    commit

--- src/main/java/dev/elma/view/IOC_IOD_Spring.java
+++ src/main/java/dev/elma/view/IOC_IOD_Spring.java
@@ -1,13 +1,15 @@
 package dev.elma.view;
 
+import dev.elma.services.IServices;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class IOC_IOD_Spring {
     public static void main(String[] args) {
-        //System.out.println(IOC_IOD_Spring.class.getResource("config"));
+        //load config file and create spring app...
         ApplicationContext applicationContext=new FileSystemXmlApplicationContext("configSp.xml");
-        System.out.println(applicationContext.getBean("service1"));
+        //getBeans ==> getObject that's have 'service1' as Name
+        IServices service=(IServices) applicationContext.getBean("service1");
     }
 }
