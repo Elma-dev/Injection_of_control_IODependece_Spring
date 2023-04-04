61be7710098aae0cb437b39e605dd01921b5005d
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-04 00:48:35 +0000

    commit

---
+++ src/main/java/dev/elma/dao/IDAOImplV2.java
@@ -0,0 +1,9 @@
+package dev.elma.dao;
+
+public class IDAOImplV2 implements IDAO{
+    @Override
+    public double getData() {
+        System.out.println("IDAOCLass V2");
+        return 12233;
+    }
+}
