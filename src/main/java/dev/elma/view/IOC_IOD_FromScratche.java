4282d94d22067b439279d0b1225e62f5919ade05
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-04 00:56:29 +0000

    commit

---
+++ src/main/java/dev/elma/view/IOC_IOD_FromScratche.java
@@ -0,0 +1,13 @@
+package dev.elma.view;
+
+import dev.elma.dao.IDAOImplV1;
+import dev.elma.services.IServicesImpl;
+
+public class IOC_IOD_FromScratche {
+    public static void main(String[] args) {
+        //create object static
+        IServicesImpl service1=new IServicesImpl();
+        service1.setDao(new IDAOImplV1());
+        System.out.println(service1.calculate());
+    }
+}
